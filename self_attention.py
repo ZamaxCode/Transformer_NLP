883b23a2958933f494a6d4211f3966c508933679
ZamaxCode <zarate.alejandroM14@live.com.mx>
2024-02-12 15:19:21 -0600

    Scaled dot product in python

--- self_attention.py
+++ self_attention.py
@@ -1,13 +1,34 @@
-## Attention in transformers
-
-#Q -> Query (What do I am looking for)
-#K -> Key (What can I offer)
-#V -> Value (What I actually offer)
-
-#d -> dimmention
-
-#Self Attention = softmax((((Q * K.T)/sqrt(d_k)) + M ) * V )
-
 import numpy as np 
 import math
 
+def softmax(x):
+    return (np.exp(x).T / np.sum(np.exp(x), axis=-1)).T
+
+def self_attention(Q,V,K, Mask=None):
+    d_k = K.shape[-1]
+    scaled = ( np.matmul(Q, K.T)/math.sqrt(d_k))
+    if Mask is not None:
+        scaled += Mask
+    attention = softmax(scaled)
+    out = np.matmul(attention, V)
+    return out
+
+def main():
+    phrase = "Hola mi nombre es Alex"
+    input_phrase = phrase.split(' ')
+    in_len = len(input_phrase)
+
+    d_K, d_V = 8, 8
+    q = np.random.randn(in_len, d_K)
+    k = np.random.randn(in_len, d_K)
+    v = np.random.randn(in_len, d_V)
+    
+    mask = np.tril(np.ones( (in_len, in_len) ))
+    mask[mask == 0] = -np.infty
+    mask[mask == 1] = 0
+
+    out = self_attention(Q=q,V=v,K=k, Mask=mask)
+
+    print(out)
+
+main()
